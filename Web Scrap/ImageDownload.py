303ef2058d421079f44a61d99daa69b6966b8311
Spoonzio <jpan569@gmail.com>
2019-08-27 15:55:31 +0200

    ImageDownload.py : changed searching method from Selenium to BSoup4, increased limit to 50

--- Web Scrap/ImageDownload.py
+++ Web Scrap/ImageDownload.py
@@ -1,50 +1,70 @@
 from bs4 import BeautifulSoup
-from selenium import webdriver
-import urllib.request
-import time
+from urllib.request import Request, urlopen
+import json, os, requests
 
-# Initiate browser
-path = "C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe"
-browser = webdriver.Chrome(path)
+# Initiate header
+header = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"}
+
+# Function to insert header in url
+def make_soup(url):
+    req_link = Request(url, headers = header)
+    return req_link
 
 # Get input from user
 term = str(input("What to search on Google images:\n"))
 
 # Get input from user, include error catching
-try:
-    count = int(input("How many images to download?\n"))
-except(TypeError):
-    print("Please enter an integer")
+count = 0
+while True:
+    try:
+        count = int(input("How many images to download? (Max 50)\n"))
+    except (ValueError):
+        print("Sorry, I didn't understand that.")
+        continue
 
-# Search on Google
-search_link = "https://www.google.com/search?q=" + term.replace(" ", "+")
-browser.get(search_link)
-links = browser.find_elements_by_css_selector("a.q.qs")
-
-# Select the Image tab
-for link in links:
-    link_text = link.text
-    if link_text == "Images":
-        link.click()
+    if count <= 0 or count > 50:
+        print("Please enter a positive nunber below 50")
+    else:
         break
-
-# Select all images
-img_links = browser.find_elements_by_css_selector(".rg_ic.rg_i")
-
-# browser.full
-
-# Click, get image and name, save
-for img_link in img_links[:count]:
-    img_link.click()
-
-    image = browser.find_element_by_css_selector("img.irc_mi")
-    img_url = image.get_attribute("src")
-
-    img_name_link = browser.find_element_by_css_selector("a.irc_pt.irc_tas.irc-cms.i3598.irc_lth")
-    img_name = img_name_link.text + ".jpeg"
-    print(str(img_url) + " # " + str(img_name))
-    urllib.request.urlretrieve(img_url, img_name)
-    print("Saved "+ img_name)
     
-# Close Browser
-browser.close()
+# Search on Google images, create soup
+search_link = "https://www.google.com/search?q="+ term.replace(" ", "+") +"&source=lnms&tbm=isch"
+print("Searching on Google images ("+ search_link+")...")
+page = urlopen(make_soup(search_link))
+soup = BeautifulSoup(page,"html.parser")
+
+# Make directory if not exist
+if not os.path.exists("Images"):
+    os.mkdir("Images")
+
+os.chdir("Images")
+
+# Find meta data from page
+flag = 0
+for meta in soup.find_all("div", {"class" : "rg_meta"}):
+    # Get img url, extension, name
+    try: 
+        img_link = json.loads(meta.text)["ou"]
+        img_ext =  json.loads(meta.text)["ity"]
+        img_name = ""
+        for char in json.loads(meta.text)["pt"]:
+            if char.isalnum() == False:
+                img_name += "-"
+            else:
+                img_name += char
+
+        if img_ext:
+            img_name += ("." + img_ext)
+            print("Saving "+ img_name +"...")
+            
+            r = requests.get(img_link)
+            with open(img_name, "wb") as imgfile:
+                imgfile.write(r.content)
+
+            flag += 1
+    except Exception:
+        pass
+
+    if flag == count:
+        print("Saved " + str(flag) +" images of " + str(term))
+        break
